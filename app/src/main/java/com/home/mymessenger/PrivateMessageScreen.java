cc56133650e25adfa7153652dcaba6821065c380
Jarno02 <jarno.ryhanen02@gmail.com>
2021-05-04 16:24:45 +0300

    created custom toolbar for private message screen

--- app/src/main/java/com/home/mymessenger/PrivateMessageScreen.java
+++ app/src/main/java/com/home/mymessenger/PrivateMessageScreen.java
@@ -1,17 +1,59 @@
 package com.home.mymessenger;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.imageview.ShapeableImageView;
 
 public class PrivateMessageScreen extends AppCompatActivity {
+
+    private ShapeableImageView shapeableImageView;
+    private TextView userNameTextView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.private_message_screen);
 
+        userNameTextView = findViewById(R.id.user_name_text_view);
+        userNameTextView.setText("Jarno");
 
+        shapeableImageView = findViewById(R.id.toolbar_shapeable_image_view);
+        shapeableImageView.setImageDrawable(getDrawable(R.drawable.common_full_open_on_phone));
 
+        setToolBar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setToolBar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
